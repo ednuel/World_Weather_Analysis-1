fbc97c578833bd3fbb94478cabc72a828390dfa9
susiexia <57109686+susiexia@users.noreply.github.com>
2019-12-23 00:15:59 -0800

    update

--- WeatherPy.py
+++ WeatherPy.py
@@ -1,8 +1,11 @@
 # %%
 import random
+from citipy import citipy
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
+
+import 
 
 # %%
 lats = np.random.uniform(-90.0,90.0,size=1500)
@@ -11,5 +14,14 @@ lngs = np.random.uniform(-180.0,180.0,size=1500)
 lats_lngs = zip(lats, lngs)
 coordinates = list(lats_lngs)
 print(coordinates[:11])
+
+# %%
+cities = list()
+for coor in coordinates:
+    cities_name = citipy.nearest_city(coor[0], coor[1]).city_name
+    # ensure no any duplicate cities
+    if cities_name not in cities:
+        cities.append(cities_name)
+print(cities[:10], 'Generate', len(cities))
 
 # %%
