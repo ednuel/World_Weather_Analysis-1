1341009d48f4dec4a540ca7a3eb2c16dd1acc25b
susiexia <57109686+susiexia@users.noreply.github.com>
2019-12-23 19:49:32 -0800

    create a iteration by enumerate function

--- WeatherPy.py
+++ WeatherPy.py
@@ -5,9 +5,12 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-import 
+import requests
+from config import weather_api_key
+from datetime import datetime
 
 # %%
+# generate 1500 random latitude and longitude data as a list of tuples
 lats = np.random.uniform(-90.0,90.0,size=1500)
 lngs = np.random.uniform(-180.0,180.0,size=1500)
 
@@ -16,6 +19,7 @@ coordinates = list(lats_lngs)
 print(coordinates[:11])
 
 # %%
+# use citipy module to get nearest city names
 cities = list()
 for coor in coordinates:
     cities_name = citipy.nearest_city(coor[0], coor[1]).city_name
@@ -23,5 +27,26 @@ for coor in coordinates:
     if cities_name not in cities:
         cities.append(cities_name)
 print(cities[:10], 'Generate', len(cities))
+
+# %%
+# loop through city names, 
+# use OpenWeather API to request, get, parse JSON to retrieve weather data for each city.
+
+# initial counters for log and sets
+record_count = 1 
+set_count = 1
+
+city_data = list()
+basic_url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=" + weather_api_key
+
+print('Beginning Data Retrieval     ')
+print("-----------------------------")
+
+# use enumerate() method to loop and build URL
+for i, item in enumerate(cities):
+    
+
+
+
 
 # %%
